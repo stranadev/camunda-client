d3fb93b586e51aaff0ddf8a0f69af458173a3708
Nikita Noskov <nik-lagan@yandex.ru>
2024-01-18 19:25:41 +0400

    Fix wrong aliases

--- camunda_client/clients/schemas.py
+++ camunda_client/clients/schemas.py
@@ -4,5 +4,5 @@ from camunda_client.types_ import BaseSchema
 
 
 class PaginationParams(BaseSchema):
-    limit: int = pydantic.Field(alias="firstResult")
-    offset: int = pydantic.Field(alias="maxResults")
+    limit: int = pydantic.Field(alias="maxResults")
+    offset: int = pydantic.Field(alias="firstResult")
